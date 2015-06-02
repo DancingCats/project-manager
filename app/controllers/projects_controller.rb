91c2a9db0a4fbfa0c899bc6975b5e8012e0491d2
Dancing cats <newsletter3132@gmail.com>
2015-06-02 02:12:15 +0000

    New start page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,9 @@
 class ProjectsController < ApplicationController
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, :alert => exception.message
+  end
+
   def new
     @project = Project.new
     authorize! :manage, @project
